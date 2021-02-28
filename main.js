d613a724f071661524d38be807b2fb8b618f8654
anvvi123 <78200009+anvvi123@users.noreply.github.com>
2021-02-28 16:00:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+var canvas=new fabric.Canvas('myCanvas');
+player_x = 10;
+player_y = 10;
+block_image_width = 30;
+block_image_height = 30;
+var player_object= "";
+var block_image_object= "";
+function player_update()
+{
+	fabric.Image.fromURL("https://i.postimg.cc/zDwfFdYY/player.png", function(Img) {
+	player_object = Img;
+player_object.scaleToWidth(150);
+	player_object.scaleToHeight(140);
+	player_object.set({
+	top:player_y,
+	left:player_x
+	});
+	canvas.add(player_object);
+  });
+}
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img) {
+	block_image_object = Img;
+block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_height);
+	block_image_object.set({
+	top:player_y,
+	left:player_x
+	});
+	canvas.add(block_image_object);
+  });
+}
+function up()
+{
+	if(player_y >=0)
+	{
+		player_y = player_y - block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When up arrow key is pressed,  x = " + player_x + " , y = " +player_y);
+		 player_update();
+	}
+}
+function down()
+{
+	if(player_y <=500)
+	{
+		player_y =player_y+ block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When down arrow key is pressed,  x = " + player_x + " , y = " +player_y);
+		player_update();
+	}
+}
+function left()
+{
+	if(player_x >= 0)
+	{
+		player_x =player_x - block_image_height;
+		console.log("block image height = " - block_image_height);
+		console.log("When left arrow key is pressed,  x = " + player_x + " , y = " +player_y);
+		player_update();
+	}
+}
+function right()
+{
+	if(player_x <= 700)
+	{
+		car2_x =car2_x + 10;
+		console.log("block image height = " - block_image_height);
+		console.log("When right arrow key is pressed,  x = " + player_x + " , y = " +player_y);
+		player_update();
+   }
+}
